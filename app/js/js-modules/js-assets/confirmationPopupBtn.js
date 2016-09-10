5dee32025db9e87b482e685ed6dd7745e351c456
Arthur <ameliepoulain-92@mail.ru>
2016-09-10 16:07:06 +0300

    remove

--- app/js/js-modules/js-assets/confirmationPopupBtn.js
+++ app/js/js-modules/js-assets/confirmationPopupBtn.js
@@ -1,8 +1,5 @@
 // confirmationPopupBtn
 confirmationPopupBtn.on('click', function(event) {
-    if ($(event.target).hasClass('confirm-block__btn--submit')) {
-        alert('submit');
-    }
     event.preventDefault();
     $(this).parents('.confirm-popup').fadeOut(500);
 });
--- app/js/main.js
+++ app/js/main.js
@@ -30,9 +30,6 @@ $(document).ready(function() {
 	// END:OPTIMIZE
 	// confirmationPopupBtn
 	confirmationPopupBtn.on('click', function(event) {
-	    if ($(event.target).hasClass('confirm-block__btn--submit')) {
-	        alert('submit');
-	    }
 	    event.preventDefault();
 	    $(this).parents('.confirm-popup').fadeOut(500);
 	});
