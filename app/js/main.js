754d8eecc1576b45fa0d1581e901f1e25391d520
Arthur <ameliepoulain-92@mail.ru>
2016-09-11 20:35:59 +0300

    popupFooterSubmitCheck done

--- app/js/main.js
+++ app/js/main.js
@@ -18,20 +18,18 @@ $(document).ready(function() {
 	function popupFooterSubmitCheck(btn, fields) {
 	    $(this).btn = btn;
 	    $(this).fields = fields;
-	    fields.each(function(index, el) {
-	        if ($(el).hasClass('active')) {
-	            btn.removeClass('disabled').addClass('active');
-	            console.log('submit is active!');
-	        } else {
-	            btn.removeClass('active').addClass('disabled');
-	        }
-	    });
+	    if (fields.hasClass('active')) {
+	        btn.removeClass('disabled').addClass('active');
+	    } else {
+	        btn.removeClass('active').addClass('disabled');
+	    }
 	}
 	// END:popupFooterSubmitCheck
 	
 	// popupTicketsOfferNumber
 	popupTicketsOfferNumber.styler({
 	    onFormStyled: function() {
+	
 	        var popupTicketsOffer = $('.popup-tickets__offer');
 	        var popupTicketsOfferBtn = popupTicketsOffer.find('.jq-number__spin');
 	        var popupTicketsOfferMinusBtn = popupTicketsOffer.find('.jq-number__spin.minus');
