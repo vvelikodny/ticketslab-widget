86ea74bd1b52a7abcca8d389ffe5bd7dc5ab42d7
Arthur <ameliepoulain-92@mail.ru>
2016-09-12 11:59:39 +0300

    dynamic starty

---
+++ app/js/js-modules/js-assets/dynamicPopupHeight.js
@@ -0,0 +1,11 @@
+// dynamicPopupHeight
+var popup = $('.popup');
+var popupTickets = popup.find('.popup-tickets');
+var windowHeight = $(window).height();			
+var popupDynamicHeight = windowHeight * 0.6652935118434603;
+var popupTicketsDynamicHeight = windowHeight * 0.34527687296416937;
+
+popup.find('.popup__container').height(popupDynamicHeight);
+popup.find('iframe').contentWindow.document.find('.popup-tickets').height(popupTicketsDynamicHeight);
+// console.log(popupDynamicHeight);
+// END:dynamicPopupHeight
