e2f989907da52eca440921f7d6ab2375db18987c
Arthur <ameliepoulain-92@mail.ru>
2016-09-12 10:10:50 +0300

    working tabindex

--- app/js/js-modules/js-assets/paymentPopup.js
+++ app/js/js-modules/js-assets/paymentPopup.js
@@ -1,11 +1,14 @@
 // paymentPopup
 paymentCardTabField.on('input', function(event) {
-	var $this = $(this);
-	var inputVal = $this.val();
-	var maxlength = $this.prop('maxlength');
+    var $this = $(this);
+    var inputVal = $this.val();
+    var maxlength = $this.prop('maxlength');
+    var inputTabIndex = $this.prop('tabindex');
 
-	if(inputVal.length === maxlength){
-		$this.nextAll('input').focus();
-	}
+
+    if (inputVal.length === maxlength) {
+        inputTabIndex++;
+        $this.parents('form').find('[tabindex="'+inputTabIndex+'"]').focus();
+    }
 });
 // END:paymentPopup
