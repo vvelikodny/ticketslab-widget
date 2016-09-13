c81a908d1fd66ff0e34f45decc99cbcc45c2f76b
Arthur <ameliepoulain-92@mail.ru>
2016-09-13 16:07:39 +0300

    mod

--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,7 @@ gulp.task('dist', function(){
 gulp.task('browserSync', function() {
 	browserSync({
 		server: {
-			baseDir: './app'
+			baseDir: './app/template-modules-mobile/index-mobile.html'
 		},
 		port: 3000,
 		open: true,
