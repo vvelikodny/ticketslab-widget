864dcb55a8acf78a1b53c3e28d621ca5e6bf078d
Arthur <ameliepoulain-92@mail.ru>
2016-09-14 11:59:49 +0300

    dinner

--- app/js/js-modules/js-assets/startPopupMobile.js
+++ app/js/js-modules/js-assets/startPopupMobile.js
@@ -1,5 +1,11 @@
 // startPopupMobile
+var popupMainHeaderInfoBtn = $('.popup-main__header-info-btn');
+var popupInfiMobile = $('.popup-info-mobile');
+
 $(document).ready(function() {
-	
+    popupMainHeaderInfoBtn.on('click', function(event) {
+        event.preventDefault();
+        popupInfiMobile.fadeIn(500);
+    });
 });
 // END:startPopupMobile
